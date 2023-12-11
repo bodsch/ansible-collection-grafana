4b5fd5c32138c2fd25d06b300b025a49737445ae
Bodo Schulz <bodo@boone-schulz.de>
2023-12-11 07:35:45 +0100

    move filter to the right position

--- plugins/filter/grafana.py
+++ plugins/filter/grafana.py
@@ -15,6 +15,7 @@ class FilterModule(object):
     def filters(self):
         return {
             'service_account_keyfiles': self.keyfiles,
+            'content_security_policy': self.content_security_policy,
         }
 
     def keyfiles(self, data):
@@ -96,4 +97,42 @@ class FilterModule(object):
 
         display.vv(f"= result: {result}")
 
+        return result
+
+
+    def content_security_policy(self, data):
+        """
+        """
+        def _string(s):
+            """
+            """
+            if s.startswith("$") or s.startswith("ws:") or s.startswith("wss:") or s.endswith(":") or s == "*" or ".com" in s:
+                return s
+            else:
+                return f"'{s}'"
+
+        result = ""
+        if isinstance(data, dict):
+            """
+                script-src 'self' 'unsafe-eval' 'unsafe-inline' 'strict-dynamic' $NONCE;
+                object-src 'none';
+                font-src 'self';
+                style-src 'self' 'unsafe-inline' blob:;
+                img-src * data:;
+                base-uri 'self';
+                connect-src 'self' grafana.com ws://$ROOT_PATH wss://$ROOT_PATH;
+                manifest-src 'self';
+                media-src 'none';
+                form-action 'self';
+            """
+            for key, value in data.items():
+                _v = []
+                for str in value:
+                    _value = _string(str)
+                    _v.append(_value)
+
+                values = " ".join(_v)
+                result += f"{key} {values}; "
+
+        # display.v(f"return : {result}")
         return result
--- plugins/filter/tools.py
+++ plugins/filter/tools.py
@@ -15,7 +15,6 @@ class FilterModule(object):
     def filters(self):
         return {
             'file_list': self.file_list,
-            'content_security_policy': self.content_security_policy,
         }
 
     def file_list(self, data):
@@ -25,41 +24,4 @@ class FilterModule(object):
         if isinstance(data, list):
             result = [x.get("path") for x in data if x.get("path")]
 
-        return result
-
-    def content_security_policy(self, data):
-        """
-        """
-        def _string(s):
-            """
-            """
-            if s.startswith("$") or s.startswith("ws:") or s.startswith("wss:") or s.endswith(":") or s == "*" or ".com" in s:
-                return s
-            else:
-                return f"'{s}'"
-
-        result = ""
-        if isinstance(data, dict):
-            """
-                script-src 'self' 'unsafe-eval' 'unsafe-inline' 'strict-dynamic' $NONCE;
-                object-src 'none';
-                font-src 'self';
-                style-src 'self' 'unsafe-inline' blob:;
-                img-src * data:;
-                base-uri 'self';
-                connect-src 'self' grafana.com ws://$ROOT_PATH wss://$ROOT_PATH;
-                manifest-src 'self';
-                media-src 'none';
-                form-action 'self';
-            """
-            for key, value in data.items():
-                _v = []
-                for str in value:
-                    _value = _string(str)
-                    _v.append(_value)
-
-                values = " ".join(_v)
-                result += f"{key} {values}; "
-
-        # display.v(f"return : {result}")
         return result
